ff86182e9b555a5d94454ee5255d4336bff7ffcd
James Makela <jamesilmarimakela@outlook.com>
2023-11-02 08:51:01 +0800

    Add employee sales class

---
+++ IOTDatabaseTraveller/DataClasses/EmployeeSales.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTDatabaseTraveller.DataClasses
+{
+    public class EmployeeSale
+    {
+        public int? ID { get; set; }
+        public string? Name { get; set; }
+        public string? ClientName { get; set; }
+        public decimal Sales { get; set; }
+
+        public EmployeeSale() { }
+
+        public WorksWith(int? id, string? name, string? clientName, decimal? sales)
+        {
+            ID = id;
+            Name = name;
+            ClientName = clientName;
+            Sales = sales;
+        }
+    }
+}
